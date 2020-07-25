e780f8448c9db302ed6fe136656c7b5cb56d0c52
Shane McDonough <shanemcdman@gmail.com>
2020-07-25 15:43:18 -0700

    Initial

---
+++ src/main.rs
@@ -0,0 +1,72 @@
+extern crate glutin_window;
+extern crate graphics;
+extern crate opengl_graphics;
+extern crate piston;
+
+use glutin_window::GlutinWindow as Window;
+use opengl_graphics::{GlGraphics, OpenGL};
+use piston::event_loop::{EventSettings, Events};
+use piston::input::{RenderArgs, RenderEvent, UpdateArgs, UpdateEvent};
+use piston::window::WindowSettings;
+
+struct Game {
+    gl: GlGraphics,
+    snake: Snake,
+}
+
+impl Game {
+    fn render(&mut self, arg: &RenderArgs){
+        const BLACK: [f32; 4] = [0.0, 0.0, 0.0, 1.0];
+        self.gl.draw(arg.viewport(), |_c, gl| {
+            graphics::clear(BLACK, gl);
+        });
+        self.snake.render(&mut self.gl, arg);
+    }
+}
+
+struct Snake {
+    pos_x: i32,
+    pos_y: i32,
+}
+
+impl Snake {
+    fn render(&self, gl: &mut GlGraphics, args: &RenderArgs){
+        const GREEN: [f32; 4] = [0.0, 1.0, 0.0, 1.0];
+        let square = graphics::rectangle::square(
+            self.pos_x as f64,
+            self.pos_y as f64,
+            10_f64
+            );
+        gl.draw(args.viewport(), |c, gl| {
+            let transform = c.transform;
+            graphics::rectangle(GREEN, square, transform, gl);
+        });
+    }
+}
+
+fn main() {
+    let opengl = OpenGL::V3_2;
+
+    let mut window: Window = WindowSettings::new(
+        "RsSnake",
+        [400, 400]
+        ).graphics_api(opengl)
+        .exit_on_esc(true)
+        .build()
+        .unwrap();
+
+    let mut game = Game{
+        gl: GlGraphics::new(opengl),
+        snake: Snake {
+            pos_x: 10,
+            pos_y: 10
+        }
+    };
+
+    let mut events = Events::new(EventSettings::new());
+    while let Some(e) = events.next(&mut window){
+        if let Some(r) = e.render_args() {
+            game.render(&r);
+        }
+    }
+}
